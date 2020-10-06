04b4a1ba5aa80c17bb1b3853dd4011f95f65992a
YuraWin <yurawin@ukr.net>
2020-10-06 16:30:15 +0300

    END comit
    END comit

---
+++ src/js/gallerymarkup.js
@@ -0,0 +1,20 @@
+export const createGalleryMarkup = function (source) {
+    return source
+        .map(({ preview, original, description }) => {
+            return `
+       <li class="gallery__item">
+     <a
+       class="gallery__link"
+       href="${original}"
+     >
+       <img
+         class="gallery__image"
+         src="${preview}"
+         data-source="${original}"
+         alt="${description}"
+       />
+     </a>
+       </li>`;
+        })
+        .join('');
+}
---
+++ src/js/index.js
@@ -0,0 +1,51 @@
+import { createGalleryMarkup } from './gallerymarkup';
+import srcGallery from './gallery-items';
+const galleryMarkup = createGalleryMarkup(srcGallery);
+
+const galleryContainer = document.querySelector('.js-gallery');
+const modalContainer = document.querySelector('.js-lightbox');
+const image = document.querySelector('.lightbox__image');
+const closeBtn = document.querySelector('button[data-action="close-lightbox"]');
+const areaOverlay = document.querySelector('div.lightbox__overlay');
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+galleryContainer.addEventListener('click', onFotoGalleryClick);
+closeBtn.addEventListener('click', onCloseModalContainer);
+areaOverlay.addEventListener('click', onCloseModalContainer);
+
+
+function onFotoGalleryClick(evt) {
+    evt.preventDefault();
+    const clickElement = evt.target;
+    if (!clickElement.classList.contains('gallery__image')) return;
+
+    const urlImage = clickElement.dataset.source;
+    const altDescription = clickElement.getAttribute('alt');
+
+    openModalContainer();
+    openImage(urlImage, altDescription);
+    addKeyEventListener();
+
+    function openModalContainer() {
+        modalContainer.classList.add('is-open');
+    }
+    function openImage(src, alt) {
+        image.src = src;
+        image.alt = alt;
+    }
+    function addKeyEventListener() {
+        window.addEventListener('keydown', onEscPress);
+    }
+}
+
+function onEscPress(evt) {
+    if (evt.code !== 'Escape') return;
+    onCloseModalContainer();
+}
+
+function onCloseModalContainer() {
+    modalContainer.classList.remove('is-open');
+    window.removeEventListener('keydown', onEscPress);
+    image.src = '';
+    image.alt = '';
+}
